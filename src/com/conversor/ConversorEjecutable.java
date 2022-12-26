d49fdf903636bde5a0da90167523f65cba1137b7
erick1231 <51278281+erick1231@users.noreply.github.com>
2022-12-25 20:20:15 -0600

    Add files via upload

---
+++ src/com/conversor/ConversorEjecutable.java
@@ -0,0 +1,12 @@
+package com.conversor;
+
+import com.gui.Ventaana;
+
+public class ConversorEjecutable {
+	public static void main(String[] args) {
+		Ventaana ventana = new Ventaana();
+		ventana.setVisible(true);
+		ventana.setLocationRelativeTo(null);
+
+	}
+}
